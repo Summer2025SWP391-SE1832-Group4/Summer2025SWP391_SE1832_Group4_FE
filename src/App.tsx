b9374744dc0f41aa4c1bd7c61bb57e0a1022f9a9
conbocuoila <phucp9698@gmail.com>
2025-05-19 11:41:04 +0700

    make login and register page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Layout from "./components/layout";
 import HomePage from "./pages/home-page";
 import './index.css'
+import LoginPage from "./pages/authentication-pages/login-page";
+import RegisterPage from "./pages/authentication-pages/register-page";
 const App = () => {
   const router = createBrowserRouter([
     {
@@ -12,8 +14,17 @@ const App = () => {
           path: "/",
           element: <HomePage />,
         }
+
       ]
     },
+    {
+      path: "/login-page",
+      element: <LoginPage />
+    },
+    {
+      path: "/register-page",
+      element: <RegisterPage />
+    }
   ]);
   return (
     <RouterProvider router={router} />
